f039f4497fbd25d1ed1bc56a4528d348084df9c6
Matt Covalt <matt@kiwiziti.com>
2022-04-05 08:40:24 -0400

    Fix signal

--- spatiotemporal/signals.py
+++ spatiotemporal/signals.py
@@ -6,7 +6,7 @@ for handling signals.
 https://docs.djangoproject.com/en/4.0/topics/signals/
 """
 from django.contrib.postgres.expressions import ArraySubquery
-from django.db.models import Coalesce, OuterRef
+from django.db.models import F
 
 from .db.functions import Box3D, MakeLine, MakePoint, XMax, XMin, YMax, YMin, ZMax, ZMin
 from .models import Coverage, Measurement
@@ -15,7 +15,23 @@ from .models import Coverage, Measurement
 def update_trajectory(sender, instance: Measurement, **kwargs):
     """Update `Coverage.trajectory` when `Measurement` is changed."""
     if sender is Measurement:
-        measurements = Measurement.objects.filter(
-            coverage_id=instance.coverage_id
-        ).annotate(bbox=Box3D())
-        Coverage.objects.filter(pk=instance.coverage_id).update(trajectory=MakeLine())
+        Coverage.objects.filter(pk=instance.coverage_id).update(
+            trajectory=MakeLine(
+                ArraySubquery(
+                    (
+                        Measurement.objects.filter(coverage_id=instance.coverage_id)
+                        .order_by("timestamp")
+                        .annotate(
+                            bbox=Box3D("geometry"),
+                            point=MakePoint(
+                                (XMin("bbox") + XMax("bbox")) / 2,
+                                (YMin("bbox") + YMax("bbox")) / 2,
+                                (ZMin("bbox") + ZMax("bbox")) / 2,
+                                F("timestamp"),
+                            ),
+                        )
+                        .values("point")
+                    )
+                )
+            )
+        )
