fbeec81c46c71afd55936ff89cc0144b7e0bd49a
Matt Covalt <matt@kiwiziti.com>
2022-04-05 11:04:55 -0400

    Register signals

--- spatiotemporal/apps.py
+++ spatiotemporal/apps.py
@@ -1,7 +1,14 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_delete, post_save
 
 
 class SpatioTemporalConfig(AppConfig):
 
     default_auto_field = "django.db.models.BigAutoField"
     name = "spatiotemporal"
+
+    def ready(self):
+        from spatiotemporal import signals
+
+        post_save.connect(signals.update_trajectory)
+        post_delete.connect(signals.update_trajectory)
