1ff958db0f3bbccd06c71f9028552edeec466bc2
Matt Covalt <matt@kiwiziti.com>
2022-04-05 07:07:50 -0400

    Remove BitField for now

--- spatiotemporal/db/fields.py
+++ spatiotemporal/db/fields.py
@@ -1,12 +1,4 @@
 from django.contrib.gis.db.models import GeometryField
-from django.db.models import Field
-
-
-class BitField(Field):
-    """A bit string field of length 1."""
-
-    def db_type(self, connection):
-        return "BIT"
 
 
 class TrajectoryField(GeometryField):
