d1293a25c5f814c053eb5438be33b6695688cf6d
Matt Covalt <matt@kiwiziti.com>
2022-04-05 12:29:45 -0400

    Add admin interface

--- spatiotemporal/admin.py
+++ spatiotemporal/admin.py
@@ -1,3 +1,23 @@
-# from django.contrib import admin
+"""Django admin registration.
 
-# Register your models here.
+This module contains registration of the application's
+automatic admin interface.
+
+https://docs.djangoproject.com/en/4.0/ref/contrib/admin/
+"""
+
+from django.contrib import admin
+
+from spatiotemporal.models import (
+    Coverage,
+    Measurement,
+    Relationship,
+    SpatialThing,
+    Universe,
+)
+
+admin.site.register(Universe)
+admin.site.register(SpatialThing)
+admin.site.register(Relationship)
+admin.site.register(Coverage)
+admin.site.register(Measurement)
