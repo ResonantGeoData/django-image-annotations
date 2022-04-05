319ee4988918a2eb257f01453a74be4f08a82cde
Matt Covalt <matt@kiwiziti.com>
2022-04-05 12:30:37 -0400

    Add URLs

--- imageannotationsd/urls.py
+++ imageannotationsd/urls.py
@@ -4,8 +4,9 @@ The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", include("spatiotemporal.urls")),
 ]
---
+++ spatiotemporal/urls.py
@@ -0,0 +1,30 @@
+"""Django URL configuration.
+
+The `urlpatterns` list routes URLs to views. This is for the
+`spatiotemporal` application.
+
+https://docs.djangoproject.com/en/4.0/topics/http/urls/
+"""
+
+
+from django.urls import include, path
+from rest_framework.routers import SimpleRouter
+
+from spatiotemporal.views import (
+    CoverageViewSet,
+    MeasurementViewSet,
+    RelationshipViewSet,
+    SpatialThingViewSet,
+    UniverseViewSet,
+)
+
+router = SimpleRouter()
+router.register(r"universes", UniverseViewSet)
+router.register(r"spatialthings", SpatialThingViewSet)
+router.register(r"relationships", RelationshipViewSet)
+router.register(r"coverages", CoverageViewSet)
+router.register(r"measurements", MeasurementViewSet)
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
